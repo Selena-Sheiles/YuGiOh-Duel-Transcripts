673a3dc7cc608ddcb059b95cfd140ff814ab84d9
Selena-Sheiles <81762440+Selena-Sheiles@users.noreply.github.com>
2021-04-05 21:13:28 +0700

    did several things

--- duel-script.js
+++ duel-script.js
@@ -17,4 +17,60 @@ function displayCard() {
     });
 }
 
-function validateCards() {} // Do nothing
+function validateCards() {
+    var cards = document.getElementsByClassName("cardName");
+    Promise.all([...cards].map(card => {
+        var url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + encodeURIComponent(card.innerHTML);
+        return fetch(url).then(response => {
+            if (response.status == 400) {
+                card.style.color = "red";
+                card.style.textDecoration = "line-through";
+                return false;
+            }
+            return response.json().then(json => {
+                var cardName = json.data[0].name;
+                if (card.innerHTML != cardName) {
+                    card.style.color = "red";
+                    return false;
+                }
+                return true;
+            });
+        });
+    })).then(arr => {
+        var isValid = arr.reduce((acc, cur) => {
+            return acc & cur;
+        }, true);
+        if (isValid)
+            alert("Cards OK");
+        else
+            alert("Cards error");
+    });
+}
+
+function checkHTML() {
+    var cards = document.getElementsByClassName("cardName");
+    Promise.all([...cards].map(card => {
+        var url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + encodeURIComponent(card.innerHTML);
+        return fetch(url).then(response => {
+            return response.json().then(json => {
+                var cardID = json.data[0].id;
+                return fetch("description/" + cardID + ".html").then(response => {
+                    if (response.status == 404) {
+                        card.style.backgroundColor = "red";
+                        card.style.color = "white";
+                        return false;
+                    }
+                    return true;
+                });
+            });
+        });
+    })).then(arr => {
+        var isValid = arr.reduce((acc, cur) => {
+            return acc & cur;
+        }, true);
+        if (isValid)
+            alert("HTMLs OK");
+        else
+            alert("HTMLs missing");
+    });
+}
