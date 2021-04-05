4af60728ceb290051c69003024bd35756d595396
Selena-Sheiles <81762440+Selena-Sheiles@users.noreply.github.com>
2021-04-05 19:36:27 +0700

    did several things

--- duel-script.js
+++ duel-script.js
@@ -17,28 +17,4 @@ function displayCard() {
     });
 }
 
-function validateCards() {
-    var cards = document.getElementsByClassName("cardName");
-    var invalid = false;
-    for (var card of cards) {
-        var url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + encodeURIComponent(card.innerHTML);
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", url, false);
-        xmlHttp.send();
-        if (xmlHttp.status == 400) {
-            card.style.color = "red";
-            card.style.textDecoration = "line-through";
-            invalid = true;
-            continue;
-        }
-        var obj = JSON.parse(xmlHttp.responseText);
-        if (card.innerHTML != obj.data[0].name) {
-            card.style.color = "red";
-            invalid = true;
-        }
-    }
-    if (invalid)
-        alert("Error");
-    else
-        alert("OK");
-}
+function validateCards() {} // Do nothing
---
+++ search-script.js
@@ -0,0 +1,41 @@
+function appendCard() {
+    event.preventDefault();
+    var cardName = this.input.value;
+    var url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + encodeURIComponent(cardName);
+    fetch(url).then(response => {
+        if (response.status == 400) {
+            alert("Card not found!");
+            return;
+        }
+        response.json().then(json => {
+            cardName = json.data[0].name;
+            getJpDesc(cardName).then(jpDesc => {
+                var card = document.createElement("li");
+                card.appendChild(document.createTextNode(cardName));
+                card.style.fontWeight = "bold";
+                card.style.color = "blue";
+                card.style.cursor = "pointer";
+                card.addEventListener("click", () => {
+                    document.getElementById("cardImage").src = json.data[0].card_images[0].image_url;
+                    document.getElementById("jpDesc").innerHTML = jpDesc;
+                });
+                var list = document.getElementById("cardNames");
+                for (var li of list.childNodes) {
+                    if (cardName < li.innerHTML) {
+                        list.insertBefore(card, li);
+                        return;
+                    }
+                    if (cardName == li.innerHTML) {
+                        alert("Duplicate!");
+                        return;
+                    }
+                }
+                list.appendChild(card);
+            });
+        });
+    });
+}
+
+function clearCards() {
+    document.getElementById("cardNames").innerHTML = "";
+}
